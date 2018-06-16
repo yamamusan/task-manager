1ddf1e576faac79cd774c2f0c8563b9e891d574a
yamamusan <fumito@yamamurafumitonoMacBook-ea.local>
2018-06-16 18:32:25 +0900

    #14 /hogeのようなURLにアクセスすると、サーバ側のルーティングが効いてしまう問題に対応

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   root to: 'home#index'
   scope :api, module: :api do
-    resources :tasks, only: [:index, :show, :create, :update, :destroy]
+    resources :tasks, only: %i[index show create update destroy]
   end
+  get '*path', to: 'home#index'
 end
